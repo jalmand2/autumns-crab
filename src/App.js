a3ca31d20d6a429d0398eeb575a8d31e4ce572db
Jessica Almand <jlalmand27@gmail.com>
2023-03-28 02:25:33 -0400

    Added text-format class for text boxes in header, fit background image for header correctly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './mainlogo.png';
+import foodBack from './images/food4.jpeg';
 import Main from './components/Main';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage: 'url(' + foodBack, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Autumn's Crab</p>
+        <h1 class="text-format">Autumn's Crab</h1>
+        <p class="text-format">Brevard County's home of delicious, affordable seafood!</p>
         
       </header>
       <Main />
